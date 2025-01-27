22547589dac7cd676dd73d67d25d1688a77058ab
Vijay Kulkarni <vijay.kulkarni@medimaze.in>
2025-01-27 22:29:40 +0530

    'auth'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
